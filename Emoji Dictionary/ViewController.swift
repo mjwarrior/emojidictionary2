4278080e5934f4633daff03e95ffea786e56b2f6
marcstreeter <mstreeter@gmail.com>
2016-10-13 11:38:35 -0500

    the emojis are here!!!

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -13,10 +13,11 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     
     @IBOutlet weak var DacoolTableView: UITableView!
    
+    var emojis = ["🐶","🐱","🐭","🐹","🐰","🐻","🐼","🐨","🐯","🦁","🐮","🐷","🐸","🐙","🐵","🐔","🐧","🐦","🐤"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    
     DacoolTableView.dataSource = self
     DacoolTableView.delegate = self
     
@@ -25,15 +26,13 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-     return 100
+     return emojis.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    
+    print(indexPath.row)
      let cell = UITableViewCell()
-     
-       cell.textLabel?.text = "Hello There!!"
-        
+     cell.textLabel?.text = emojis[indexPath.row]
         return cell
     }
     
